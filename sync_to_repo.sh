b8569c1692a936e11c58f2354f352d70bc776b03
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-12-14 02:08:58 -0800

    Create sync_to_repo.sh

---
+++ sync_to_repo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Moving dot-files from local to repo"
+
+echo "Moving .vimrc..."
+cp ~/.vimrc ./
+echo "Done"
+
+echo "moving .spacemacs..."
+cp ~/.spacemacs ./
+echo "done"
+
+echo "moving .gitconfig..."
+cp ~/.gitconfig ./
+echo "done"
+
+echo "moving .zshrc..."
+cp ~/.zshrc ./
+echo "done"
+
+echo "moving .zpreztorc..."
+cp ~/.zpreztorc ./
+echo "done"
+
+echo "moving .zprofile..."
+cp ~/.zprofile ./
+echo "done"
+
+echo "moving .bashrc..."
+cp ~/.bashrc ./
+echo "done"
+
+echo "moving .bash_profile..."
+cp ~/.bash_profile ./
+echo "done"
+
+echo "moving .git_cleanup.bash..."
+cp ~/.git_cleanup.bash ./
+echo "done"
